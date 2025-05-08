339fa44912a450111ab50c527c1c13bd095fbf84
Mona Lisa <Mona Lisa>
2025-05-08 19:55:45 +0900

    ゲーム画面のセンタリングと下部への説明文移動を実装

--- b_shot/b_shot.js
+++ b_shot/b_shot.js
@@ -2292,8 +2292,8 @@ function initSync(module) {
 
 async function __wbg_init(module_or_path) {
     if (wasm !== undefined) return wasm;
-
-
+    
+    
     if (typeof module_or_path !== 'undefined') {
         if (Object.getPrototypeOf(module_or_path) === Object.prototype) {
             ({module_or_path} = module_or_path)
@@ -2301,21 +2301,34 @@ async function __wbg_init(module_or_path) {
             console.warn('using deprecated parameters for the initialization function; pass a single object instead')
         }
     }
-
+    
     if (typeof module_or_path === 'undefined') {
         module_or_path = new URL('b_shot_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
-
+    
     if (typeof module_or_path === 'string' || (typeof Request === 'function' && module_or_path instanceof Request) || (typeof URL === 'function' && module_or_path instanceof URL)) {
         module_or_path = fetch(module_or_path);
     }
-
+    
     __wbg_init_memory(imports);
-
+    
     const { instance, module } = await __wbg_load(await module_or_path, imports);
+    
+    __wbg_finalize_init(instance, module);
 
-    return __wbg_finalize_init(instance, module);
+    
+     // キャンバス要素をgame-container内に配置
+     const canvas = document.querySelector('canvas');
+     if (canvas) {
+        const gameContainer = document.getElementById('game-container');
+        if (gameContainer) {
+            gameContainer.appendChild(canvas);
+        }
+    }
+
+
+    return wasm;
 }
 
 export { initSync };
